510d3161fcaea3f0f8e1491c16371a5a3d40ade5
tamagsantos <tamarags.ve@hotmail.com>
2025-03-07 22:20:53 -0300

    Iniciando a interface gráfica

--- MeuAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MeuAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace MeuAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
@@ -5,5 +7,16 @@ public partial class ListaProduto : ContentPage
 	public ListaProduto()
 	{
 		InitializeComponent();
+	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+		}catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
 	}
 }
--- MeuAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MeuAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MeuAppMinhasCompras.Models;
+
 namespace MeuAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,23 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+		}catch(Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
 }
